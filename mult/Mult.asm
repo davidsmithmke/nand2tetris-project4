2cad9953b899338130ccebb3f4c1a24bf17207b8
David Smith <dsmith@davidsmithmke.com>
2014-02-05 09:45:32 -0600

    Add more robust comments to Mult.asm
    Add more robust comments to multiplication program to explain the overall
    structure of the program.

--- mult/Mult.asm
+++ mult/Mult.asm
@@ -6,6 +6,13 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
+// R0 will be added to itself R1 times, the result being stored in R2.
+// R1 is used as a counter, and decremented each time through the loop.  The
+// addition loop completes when R1 is less than or equal to 0.  (R1 should never
+// actually reach a point where it is less than 0, this is merely a safeguard.)
+// R2 holds the sum at the current stage of the loop (and thus the final
+// product once the loop has completed).
+
 (BEGIN)
 	// Initialize product to 0.
 	@R2
