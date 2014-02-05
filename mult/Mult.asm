6a041fbd56dd85c912221e4b31533a96f61549cd
David Smith <dsmith@davidsmithmke.com>
2014-02-05 09:39:51 -0600

    Implement multiplication program

--- mult/Mult.asm
+++ mult/Mult.asm
@@ -6,4 +6,28 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
-// Put your code here.
+(BEGIN)
+	// Initialize product to 0.
+	@R2
+	M=0
+(LOOP)
+	// If R1 <= 0, terminate.
+	@R1
+	D=M
+	@END
+	D;JLE
+	// Add R0 to R2.
+	@R0
+	D=M
+	@R2
+	M=M+D
+	// Decerement R1.
+	@R1
+	M=M-1
+	// Loop.
+	@LOOP
+	0;JMP
+(END)
+	// Terminate.
+	@END
+	0;JMP
