b9700d4b8adf9ab67aec837990f6646537ee3745
David Smith <dsmith@davidsmithmke.com>
2014-02-05 11:01:47 -0600

    Implement screen fill program
    Implement screen fill program using separate loops to fill and clear
    screen.

--- fill/Fill.asm
+++ fill/Fill.asm
@@ -8,4 +8,62 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+(BEGIN)
+	// Set keyboard to address for keyboard input value.
+	@24576
+	D=A
+	@keyboard
+	M=D
+	// Set current to last screen pixel map.
+	@24575
+	D=A
+	@current
+	M=D
+(CHECK_KEYBOARD)
+	// If keyboard is pressed, fill the screen.
+	@keyboard
+	A=M
+	D=M
+	@FILL
+	D;JNE
+	// Otherwise, clear the screen.
+	@CLEAR
+	0;JMP
+(FILL)
+	// Fill current pixel.
+	@current
+	A=M
+	M=-1
+	// If current pixel map is first pixel map there is nothing left to fill, so
+	//jump back to keyboard check.
+	@current
+	D=M
+	@16384
+	D=D-A
+	@CHECK_KEYBOARD
+	D;JLE
+	// Decrement current pixel map.
+	@current
+	M=M-1
+	// Continue filling next pixel map.
+	@FILL
+	0;JMP
+(CLEAR)
+	// Clear current pixel.
+	@current
+	A=M
+	M=0
+	// If current pixel map is last pixel map there is nothing left to clear, so
+	// jump back to keyboard check.
+	@current
+	D=M
+	@24575
+	D=D-A
+	@CHECK_KEYBOARD
+	D;JGE
+	// Increment current pixel map.
+	@current
+	M=M+1
+	// Continue clearing next pixel map.
+	@CLEAR
+	0;JMP
